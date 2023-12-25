7308d58baa208a6c4d21c67dabb8e0e4d1ded4de
MaiaKiflawi <MaiaKiflawi@gmail.com>
2023-12-25 14:55:58 +0200

    Add project files.

---
+++ WpfClientCalander/LoginWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfClientCalander.ServiceCalander;
+
+namespace WpfClientCalander
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private CalanderServiceClient serviceCal;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            serviceCal = new CalanderServiceClient();
+            
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
