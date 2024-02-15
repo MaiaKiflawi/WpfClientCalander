db23fe24b7e276d3831b7bd82b906209e13f11d1
MaiaKiflawi <MaiaKiflawi@gmail.com>
2024-02-15 09:07:49 +0200

    ..

---
+++ WpfClientCalander/IconConverter.cs
@@ -0,0 +1,40 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfClientCalander
+{
+    public class IconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return PackIconKind.Abc;
+            switch (value)
+            {
+                case "Home":
+                    return PackIconKind.Home;
+                case "Group Admin Page":
+                    return PackIconKind.TableAccount;
+                case "Manager Settings":
+                    return PackIconKind.AccountSupervisorCircle;
+                case "My Profile":
+                    return PackIconKind.HomeAccount;
+                default:
+                        break;
+                }
+            return PackIconKind.Abc;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfClientCalander/UserWindow.xaml.cs
+++ WpfClientCalander/UserWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,18 +23,21 @@ namespace WpfClientCalander
     public partial class UserWindow : Window
     {
         private Users user;
-
+        private GroupsList myGroups;
+        private CalanderServiceClient serviceClient;
         public UserWindow(Users user)
         {
             InitializeComponent();
             this.user = user;
+            serviceClient = new CalanderServiceClient();
+            myGroups= serviceClient.GetGroupsByUser(user);
             if (user.IsManager)
             {
                 rdbHome.Visibility = Visibility.Visible;
                 rdbGroupAdmin.Visibility = Visibility.Visible;
                 rdbManagerSettings.Visibility = Visibility.Visible;
                 rdbMyProfile.Visibility = Visibility.Visible;
-                
+
             }
             if (user.IsGroupAdmin)
             {
@@ -50,6 +54,64 @@ namespace WpfClientCalander
                 rdbManagerSettings.Visibility = Visibility.Collapsed;
                 rdbMyProfile.Visibility = Visibility.Visible;
             }
+
+            GroupsView.Children.Clear();
+            foreach (Groups group in myGroups) 
+            {
+                StackPanel stackPanel = new StackPanel();
+                stackPanel.Orientation=Orientation.Horizontal;
+
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = group.GroupName;
+
+                Ellipse ellipse = new Ellipse();
+                ellipse.VerticalAlignment = VerticalAlignment.Center;
+                ellipse.HorizontalAlignment = HorizontalAlignment.Center;
+                ellipse.Height = ellipse.Width = 100;
+                ellipse.Margin = new Thickness(5);
+                ImageBrush imageBrush = GetGroupImage(group.GroupName);
+                ellipse.Fill = imageBrush;
+
+                stackPanel.Children.Add(ellipse);
+                stackPanel.Children.Add(textBlock);
+                stackPanel.Tag = group;
+                stackPanel.MouseDown += StackPanel_MouseDown;
+                GroupsView.Children.Add(stackPanel);
+            }
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            StackPanel stackPanel= (StackPanel)sender;
+            Groups group=stackPanel.Tag as Groups;
+
+        }
+
+        private ImageBrush GetGroupImage(string groupName)
+        {
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.Stretch = Stretch.UniformToFill;
+            string uriStr = Environment.CurrentDirectory; //המיקום שבו רץ הפרויקט
+            uriStr = uriStr.Substring(0, uriStr.IndexOf("\\bin"));
+            uriStr = uriStr + @"\Images\imgGroups\";
+            BitmapImage bitmapImage = new BitmapImage();
+            DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(uriStr);
+            FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles(groupName + ".*");
+            if (filesInDir.Length > 0)
+            {
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri(filesInDir[0].FullName);
+                bitmapImage.EndInit();
+                imageBrush.ImageSource = bitmapImage;
+            }
+            else
+            {
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri(@"pack://application:,,,/Images/projectLogo.jpg");
+                bitmapImage.EndInit();
+                imageBrush.ImageSource = bitmapImage;
+            }
+            return imageBrush;
         }
 
         private void Themes_Click(object sender, RoutedEventArgs e)
@@ -101,6 +163,11 @@ namespace WpfClientCalander
         {
             ucGrid.Children.Clear();
             ucGrid.Children.Add(new UserProfileUC(user));
+        }
+
+        private void rdHome_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
