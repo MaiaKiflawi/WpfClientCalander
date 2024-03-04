991a068b000a73bbfb509e0f4fe79ded8961df16
MaiaKiflawi <MaiaKiflawi@gmail.com>
2024-03-04 16:07:01 +0200

    calendar design

--- WpfClientCalander/Window1.xaml.cs
+++ WpfClientCalander/Window1.xaml.cs
@@ -31,13 +31,80 @@ namespace WpfClientCalander
 
         private void SetGridLines()
         {
-            calGrid.ShowGridLines = false;
-            calGrid.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3C, 0x29, 0x29));
-            for (int i = 0; i < 11; i++)
+            //calGrid.ShowGridLines = false;
+            for (int i = 1; i <= 11; i++)
             {
-                for (int j = 0; j < 7; j++)
+                for (int j = 1; j <= 7; j++)
                 {
-                    //calGrid. set border colour
+                    if (i == 1)
+                    {
+                        Border myBorder = new Border();
+                        myBorder.Height = 20;
+                        myBorder.Width = 830 / 7;
+                        myBorder.Background = new SolidColorBrush(Color.FromArgb(255, 0xc0, 0xb3, 0xc2));
+                        myBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 0x7e, 0x4c, 0x45));
+                        myBorder.BorderThickness = new Thickness(1);
+                        myBorder.Margin = new Thickness(.3);
+                        calWP.Children.Add(myBorder);
+                        //Grid.SetRowSpan(myBorder, i);
+                        //Grid.SetColumnSpan(myBorder, j);
+                    }
+                    else if (i % 2 == 0)
+                    {
+                        Border myBorder = new Border();
+                        myBorder.Height = 20;
+                        myBorder.Width = 830 / 7;
+                        myBorder.Background = new SolidColorBrush(Color.FromArgb(255, 0xf3, 0xe5, 0xf5));
+                        myBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 0x7e, 0x4c, 0x45));
+                        myBorder.BorderThickness = new Thickness(1);
+                        myBorder.Margin = new Thickness(.3);
+                        calWP.Children.Add(myBorder);
+                        //Grid.SetRowSpan(myBorder, i);
+                        //Grid.SetColumnSpan(myBorder, j);
+                    }
+                    else
+                    {
+                        Border myBorder = new Border();
+                        myBorder.Height = 60;
+                        myBorder.Width = 830 / 7;
+                        myBorder.Background = new SolidColorBrush(Color.FromArgb(255, 0xff, 0xf3, 0xe0));
+                        myBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 0x7e, 0x4c, 0x45));
+                        myBorder.BorderThickness = new Thickness(1);
+                        myBorder.Margin = new Thickness(.3);
+                        calWP.Children.Add(myBorder);
+                        //Grid.SetRowSpan(myBorder, i);
+                        //Grid.SetColumnSpan(myBorder, j);
+                    }
+                    //if (i == 1)
+                    //{
+                    //    Border myBorder = new Border();
+                    //    myBorder.Background = new SolidColorBrush(Color.FromArgb(255, 0xc0, 0xb3, 0xc2));
+                    //    myBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 0x7e, 0x4c, 0x45));
+                    //    myBorder.BorderThickness = new Thickness(2);
+                    //    calGrid.Children.Add(myBorder);
+                    //    Grid.SetRowSpan(myBorder, i);
+                    //    Grid.SetColumnSpan(myBorder, j);
+                    //}
+                    //else if (i%2 == 0)
+                    //{
+                    //    Border myBorder = new Border();
+                    //    myBorder.Background = new SolidColorBrush(Color.FromArgb(255, 0xf3, 0xe5, 0xf5));
+                    //    myBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 0x7e, 0x4c, 0x45));
+                    //    myBorder.BorderThickness = new Thickness(2);
+                    //    calGrid.Children.Add(myBorder);
+                    //    Grid.SetRowSpan(myBorder, i);
+                    //    Grid.SetColumnSpan(myBorder, j);
+                    //}
+                    //else
+                    //{
+                    //    Border myBorder = new Border();
+                    //    myBorder.Background = new SolidColorBrush(Color.FromArgb(255, 0xff, 0xf3, 0xe0));
+                    //    myBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 0x7e, 0x4c, 0x45));
+                    //    myBorder.BorderThickness = new Thickness(2);
+                    //    calGrid.Children.Add(myBorder);
+                    //    Grid.SetRowSpan(myBorder, i);
+                    //    Grid.SetColumnSpan(myBorder, j);
+                    //}
                 }
 
             }
